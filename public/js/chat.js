3594dcc529c766cee062508d4cf28cde735cbbe0
hyper-vlados <hypervlados@gmail.com>
2020-12-10 14:50:33 +0200

    Send Message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,27 @@
 const connect = new WebSocket('ws://127.0.0.1:8888');
 
-connect.onopen = (e) => {
-    console.log('Connected');
+let userName = prompt('Your name:');
+
+connect.onopen = () => {
+    console.log(`New user ${userName} connected`);
 }
 
 connect.onmessage = (e) => {
-    console.log('Get data ' + e.data);
+    console.log(`${e.data}`);
+}
+
+const getDate = () => {
+    let today = new Date();
+    let date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    let time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    let dateTime = `${date} ${time}`;
+
+    return dateTime;
 }
 
 function send() {
-    let message = "hello for you ";
+    let userText = prompt();
+    let message = `-${userName}: ${userText} [${getDate()}]`;
     connect.send(message);
-    console.log(' ' + message + ' was send');
+    console.log(message);
 }
