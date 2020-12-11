a254a8176046a419853fd12ce5190e0844d7e05f
hyper-vlados <hypervlados@gmail.com>
2020-12-11 16:27:25 +0200

    Socket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,6 @@
 <div class="container-fluid h-100">
     <div class="row justify-content-center h-100">
         <div class="col-md-4 col-xl-3 chat"><div class="card mb-sm-3 mb-md-0 contacts_card">
-                <div class="card-header">
-                    <div class="input-group">
-                        <input type="text" placeholder="Search..." class="form-control search">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
-                        </div>
-                    </div>
-                </div>
                 <div class="card-body contacts_body">
                     <ul class="contacts">
                         <li class="active">
